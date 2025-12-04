a22a5f748320ee08e68be00f15c7a5ec2f8f5062
bosskopp <1733427+bosskopp@users.noreply.github.com>
2025-12-04 11:16:31 +0000

    add storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,7 @@ Rails.application.configure do
 
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Storage
+  config.active_storage.service = :production
 end
